238a3248a1f02295c518e1869be594e2fdbbaa32
ElInfiltradorDeCodigo <perrokasper2003@gmail.com>
2023-11-22 12:20:30 -0600

    Mejoras Clientes

--- clientes.php
+++ clientes.php
@@ -50,7 +50,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
         if (empty($_POST['correo']) || !filter_var($_POST['correo'], FILTER_VALIDATE_EMAIL)) {
             $errores[] = 'Se requiere un correo electrónico válido';
         }
-        if (empty($_POST['teléfono'])) {
+        if (empty($_POST['telefono'])) {
             $errores[] = 'El campo teléfono es obligatorio';
         }
         if (strlen($_POST['calle']) > 80) {
@@ -75,63 +75,36 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $foto = file_get_contents($_FILES['foto']['tmp_name']);
         }
 
-        // Preparación de la consulta
-        $stmt = $conn->prepare("INSERT INTO clientes (nombre, correo, teléfono, foto, calle, colonia, codigo_postal) VALUES (?, ?, ?, ?, ?, ?, ?)");
-        $stmt->bind_param("sssbsss", $_POST['nombre'], $_POST['correo'], $_POST['teléfono'], $foto, $_POST['calle'], $_POST['colonia'], $_POST['codigo_postal']);
-
-        if ($foto !== null) {
-            $stmt->send_long_data(3, $foto);
+        if (isset($_POST['idCliente'])) {
+            // Actualización del cliente
+            $idCliente = $conn->real_escape_string($_POST['idCliente']);
+            $sql = "UPDATE clientes SET nombre = ?, correo = ?, telefono = ?, calle = ?, colonia = ?, codigo_postal = ?" . ($foto !== null ? ", foto = ?" : "") . " WHERE idCliente = ?";
+            $stmt = $conn->prepare($sql);
+            if ($foto !== null) {
+                $stmt->bind_param("ssssssbi", $_POST['nombre'], $_POST['correo'], $_POST['telefono'], $_POST['calle'], $_POST['colonia'], $_POST['codigo_postal'], $foto, $idCliente);
+                $stmt->send_long_data(6, $foto);
+            } else {
+                $stmt->bind_param("ssssssi", $_POST['nombre'], $_POST['correo'], $_POST['telefono'], $_POST['calle'], $_POST['colonia'], $_POST['codigo_postal'], $idCliente);
+            }
+        } else {
+            // Inserción de un nuevo cliente
+            $stmt = $conn->prepare("INSERT INTO clientes (nombre, correo, telefono, foto, calle, colonia, codigo_postal) VALUES (?, ?, ?, ?, ?, ?, ?)");
+            $stmt->bind_param("sssbsss", $_POST['nombre'], $_POST['correo'], $_POST['telefono'], $foto, $_POST['calle'], $_POST['colonia'], $_POST['codigo_postal']);
+            if ($foto !== null) {
+                $stmt->send_long_data(3, $foto);
+            }
         }
 
         // Ejecución y respuesta
         if ($stmt->execute()) {
-            $data = [
-                'idCliente' => $stmt->insert_id,
-                'nombre' => $_POST['nombre'],
-                'correo' => $_POST['correo'],
-                'teléfono' => $_POST['teléfono'],
-                'calle' => $_POST['calle'],
-                'colonia' => $_POST['colonia'],
-                'codigo_postal' => $_POST['codigo_postal']
-                // Puedes añadir aquí otros campos si los agregas en el futuro
-            ];
-            sendJsonResponse('success', $data, 'Cliente agregado con éxito');
+            $response = isset($_POST['idCliente']) ? 'Cliente actualizado con éxito' : 'Cliente agregado con éxito';
+            $data = ['idCliente' => isset($idCliente) ? $idCliente : $stmt->insert_id];
+            sendJsonResponse('success', $data, $response);
         } else {
-            sendJsonResponse('error', null, 'Error al insertar el cliente');
+            $error = isset($_POST['idCliente']) ? 'Error al actualizar el cliente' : 'Error al insertar el cliente';
+            sendJsonResponse('error', null, $error);
         }
         $stmt->close();
-        break;
-
-
-    case 'PUT':
-        if (isset($_GET['idCliente'])) {
-            parse_str(file_get_contents("php://input"), $postData);
-            $idCliente = $conn->real_escape_string($_GET['idCliente']);
-
-            if (empty($postData['nombre']) || empty($postData['correo']) || empty($postData['telefono'])) {
-                sendJsonResponse('error', null, 'Todos los campos son obligatorios');
-                break;
-            }
-
-            $sql = "UPDATE clientes SET nombre = ?, correo = ?, telefono = ?, calle = ?, colonia = ?, codigo_postal = ?";
-            if (isset($_FILES['foto'])) {
-                $foto = file_get_contents($_FILES['foto']['tmp_name']);
-                $sql .= ", foto = ?";
-                $stmt = $conn->prepare($sql);
-                $stmt->bind_param("ssssssb", $postData['nombre'], $postData['correo'], $postData['telefono'], $postData['calle'], $postData['colonia'], $postData['codigo_postal'], $foto);
-            } else {
-                $stmt = $conn->prepare($sql);
-                $stmt->bind_param("ssssss", $postData['nombre'], $postData['correo'], $postData['telefono'], $postData['calle'], $postData['colonia'], $postData['codigo_postal']);
-            }
-
-            $stmt->execute();
-
-            if ($stmt->affected_rows > 0) {
-                sendJsonResponse('success', null);
-            } else {
-                sendJsonResponse('error', null, 'Error al actualizar el cliente');
-            }
-        }
         break;
 
     case 'DELETE':
